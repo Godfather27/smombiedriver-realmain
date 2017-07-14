ed45d4c2db2f53251ef27cb39fb0454fd35bd023
Lars Engel <lars.engel@innogames.com>
2017-07-14 22:22:05 +0200

    fe app calls to create and join rooms

---
+++ client/rooms.js
@@ -0,0 +1,25 @@
+function createRoom() {
+    Meteor.call('createRoom', (err, res) => {
+        if (err) {
+            console.log(err.message);
+        } else {
+            console.log(res);
+            joinRoom(res);
+        }
+    });
+}
+
+
+function joinRoom(room_id) {
+    Meteor.call('joinRoom',
+        {room_id: room_id},
+        (err, res) => {
+            if (err) {
+                console.log(err.message);
+            } else {
+                console.log(res);
+            }
+        });
+}
+
+createRoom();
