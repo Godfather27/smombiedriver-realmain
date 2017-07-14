993e592696ee61309c24a1d4991dcc87f4820336
Lars Engel <lars.engel@innogames.com>
2017-07-14 22:20:39 +0200

    be method to join rooms

--- server/methods.js
+++ server/methods.js
@@ -1,9 +1,19 @@
 Meteor.methods({
-    'createRoom' : function() {
+    'createRoom'() {
         var room_id = makeId();
         Rooms.insert({room_id : room_id, app_connected: false});
         console.log("Room created: " + room_id);
         return room_id;
+    },
+    'joinRoom'({room_id}) {
+        console.log("Trying to connect to room with id: " + room_id);
+        var room = Rooms.findOne({room_id: room_id});
+        if (room) {
+            Rooms.update({room_id: room_id}, { $set: { app_connected: true } });
+            return true;
+        } else {
+            throw new Meteor.Error("Room not found");
+        }
     }
 });
 
