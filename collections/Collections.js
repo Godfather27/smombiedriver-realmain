e304e1df4f2857831b35da49ed0e8e80bcf54f65
Lars Engel <lars.engel@innogames.com>
2017-07-14 22:45:36 +0200

    structure of rooms

--- collections/Collections.js
+++ collections/Collections.js
@@ -2,7 +2,7 @@
 // {_id: UUID, question: string, answer: string}
 Dialog = new Mongo.Collection('dialog');
 
-// {_id_ UUID, room_id: random_string}
+// {_id_ UUID, room_id: random_string, app_connected: bool}
 Rooms = new Mongo.Collection('rooms');
 
 // {_id: UUID, room_id: fk.rooms, points: int}
