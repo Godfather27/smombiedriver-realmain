2388846add9053670aef7cd94593dc3b9602eadd
Lars Engel <lars.engel@innogames.com>
2017-07-15 01:06:00 +0200

    add method to check input of user

--- client/lobby.js
+++ client/lobby.js
@@ -11,5 +11,22 @@ Template.lobby.events({
                     Session.set("room_id", room_id);
                 }
             });
+    },
+    'click #lobby_add_points': function(){
+        Meteor.call('checkAnswer',
+            {answer_id: "ABC", input_answer: "", room_id: Session.get("room_id")},
+            (err, res) => {
+                if (err) {
+                    console.log(err.message);
+                } else {
+                    console.log(res);
+                }
+            });
+    }
+});
+
+Template.lobby.helpers({
+    'joined_room': function() {
+        return Session.get("room_id");
     }
 });
